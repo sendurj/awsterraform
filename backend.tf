fbd2c1bcaa36083df068ef816fb0a80bc0fc2c0d
sendurj <166694939+sendurj@users.noreply.github.com>
2024-04-13 20:34:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-s3-bucket-for-tf-state-file1"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "my-dynamo-db-table-tf-file1"
   }
 }
